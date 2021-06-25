645abcf32b78ec2aae9a8fdee2f7014edec55376
Mairton Damasceno <mairtondamasceno@gmail.com>
2021-06-25 16:14:48 -0300

    Bloco 4, dia 03. Exercicio 01

---
+++ bloco_4/dia_3/exercicio01.js
@@ -0,0 +1,10 @@
+let n = 4;
+let symbol = "*";
+let inputLine = "";
+
+for(let index=0; index<n; index+=1){
+  inputLine = symbol + inputLine;
+}
+for(let index=0; index<n; index+=1){
+console.log(inputLine)
+}
